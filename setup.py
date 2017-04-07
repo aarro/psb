d8862a2fcc769e7f34ca16e82afd7d3045ef8b59
Jacob Young <enderwggn28@gmail.com>
2017-04-07 14:29:03 -0500

    Adding standard python things for packages (setup, dirs, etc)

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+PSB, a bot to scan local LAN for a whitelist of macs to report presence.
+"""
+import os.path
+from setuptools import setup
+
+ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+
+setup(name='psb',
+      version='0.1',
+      description='A bot to scan local LAN for a whitelist of macs to report presence.',
+      url='https://github.com/aarro/psb',
+      author='Jacob Young',
+      author_email='jacobyoung@uta.io',
+      long_description=open(os.path.join(ROOT, 'README.md')).read(),
+      license='MIT',
+      packages=['psb'],
+      install_requires=[
+          'python-dotenv',
+          'slackclient'
+      ],
+      zip_safe=False)
