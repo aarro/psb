4245c5d4c05cc33521525a233955d8a4d8a66135
Jacob Young <enderwggn28@gmail.com>
2017-04-18 12:05:26 -0500

    using threading.Lock and cleaning up some code

--- psb/psb.py
+++ psb/psb.py
@@ -5,7 +5,7 @@ import platform
 import re
 import subprocess
 from time import sleep
-from threading import Thread
+from threading import Thread, Lock
 from slackclient import SlackClient
 from dotenv import load_dotenv, find_dotenv
 from .classes import Device
@@ -23,9 +23,16 @@ GROUP_ID = os.environ.get("SLACK_GROUP")
 SLACK_CLIENT = SlackClient(SLACK_TOKEN)
 
 # Other
+LOCK = Lock()
+SKIP_SLACK = os.environ.get("SKIP_SLACK") or False
 MAC_REGEX = "[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$"
 IP_REGEX = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}" \
            r"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+
+def call_slack(message):
+    """Sends a message to slack"""
+    if not SKIP_SLACK:
+        SLACK_CLIENT.api_call("chat.postMessage", channel=GROUP_ID, text=message, as_user=True)
 
 def who_is_here(who):
     """Function that checks for device presence"""
@@ -37,47 +44,45 @@ def who_is_here(who):
 
     # Loop through checking for devices and counting if they're not present
     while True:
-        if STOP:
-            print "Exiting Thread - " + o_name
-            exit()
-        else:
-            pass
+        with LOCK:
+            if STOP:
+                print "Exiting Thread - " + o_name
+                exit()
 
-        # If a listed device address is present print and stream
-        if o_addr in DEVICE_DICT:
-            device = DEVICE_DICT[o_addr]
-            if device.online:
-                if device.is_active(30): # 30 second grace period for not seeing a device
-                    print device.name + ' is here (arp-scan)'
-                else: # After 30 seconds check the mac/ip directly
-                    found = False
-                    arping = "sudo arping -c 3 -t " + device.mac + " " + device.ipv4
-                    process = subprocess.Popen(arping, stdout=subprocess.PIPE,
-                                               stderr=subprocess.PIPE, shell=True)
-                    a_out, err = process.communicate()
-                    if err:
-                        print err
-                    else:
-                        a_lines = a_out.splitlines(False)
-                        for a_l in a_lines:
-                            ping = a_l.split(" ")
-                            if len(ping) > 3:
-                                device.seen()
-                                found = True
-                                break
-                        if found:
-                            print device.name + ' is here (arping)'
-                        else:
-                            device.online = False
-                            SLACK_CLIENT.api_call("chat.postMessage", channel=GROUP_ID,
-                                                  text=device.name + " is gone", as_user=True)
-            o_online = device.online
-
-        if not o_online:
-            last_seen = "never"
+            # If a listed device address is present print and stream
             if o_addr in DEVICE_DICT:
-                last_seen = DEVICE_DICT[o_addr].last_seen.strftime("%Y-%m-%d %H:%M:%S")
-            print o_name + " is not here. Last seen " + last_seen
+                device = DEVICE_DICT[o_addr]
+                if device.online:
+                    if device.is_active(30): # 30 second grace period for not seeing a device
+                        print device.name + ' is here (arp-scan)'
+                    else: # After 30 seconds check the mac/ip directly
+                        found = False
+                        arping = "sudo arping -c 3 -t " + device.mac + " " + device.ipv4
+                        process = subprocess.Popen(arping, stdout=subprocess.PIPE,
+                                                   stderr=subprocess.PIPE, shell=True)
+                        a_out, err = process.communicate()
+                        if err:
+                            print err
+                        else:
+                            a_lines = a_out.splitlines(False)
+                            for a_l in a_lines:
+                                ping = a_l.split(" ")
+                                if len(ping) > 3:
+                                    device.seen()
+                                    found = True
+                                    break
+                            if found:
+                                print device.name + ' is here (arping)'
+                            else:
+                                device.online = False
+                                call_slack(device.name + " is gone")
+                o_online = device.online
+
+            if not o_online:
+                last_seen = "never"
+                if o_addr in DEVICE_DICT:
+                    last_seen = DEVICE_DICT[o_addr].last_seen.strftime("%Y-%m-%d %H:%M:%S")
+                print o_name + " is not here. Last seen " + last_seen
         sleep(10)
 
 # Main thread
@@ -86,7 +91,7 @@ try:
     STOP = False
     DEVICE_DICT = {}
 
-    SLACK_CLIENT.api_call("chat.postMessage", channel=GROUP_ID, text='Starting up', as_user=True)
+    call_slack("Starting up")
 
     # Start the thread(s)
     for i in range(len(DATA)):
@@ -99,7 +104,7 @@ try:
         A_SPLIT = " "
         A_MAC_INDEX = 3
         A_IP_INDEX = 1
-
+ 
         PLATFORM = platform.system()
         # linux
         if PLATFORM == "Linux":
@@ -116,15 +121,19 @@ try:
                 mac = conns[A_MAC_INDEX].upper()
                 # mac arp-scan wraps the ip in parans
                 ip_addr = re.search(IP_REGEX, conns[A_IP_INDEX]).group(0)
+
+                # if we have a mac not already captured, see if we should
                 if mac not in DEVICE_DICT:
                     for d in DATA:
                         if d["mac"] == mac:
                             DEVICE_DICT[mac] = Device(d["name"], d["mac"])
+                            break
+
+                # if mac is tracked, update the device
                 if mac in DEVICE_DICT:
                     d = DEVICE_DICT[mac]
                     if not d.online:
-                        SLACK_CLIENT.api_call("chat.postMessage", channel=GROUP_ID,
-                                              text=d.name + " is here", as_user=True)
+                        call_slack(d.name + " is here")
                     DEVICE_DICT[mac].seen(ipv4=ip_addr)
 
         # Wait 5 seconds between arp-scans
@@ -133,6 +142,6 @@ try:
 except KeyboardInterrupt:
     # On a keyboard interrupt signal threads to exit
     STOP = True
-    SLACK_CLIENT.api_call("chat.postMessage", channel=GROUP_ID, text='Shutting down', as_user=True)
+    call_slack("Shutting down")
     sleep(30)
     exit()
